922c23f0c29bc32ab896b6b35055e435de31fa77
nigeiru <158778430+nigeiru@users.noreply.github.com>
2024-07-22 23:15:09 +0800

    数据库更新

--- takeaway.sql
+++ takeaway.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80031
  File Encoding         : 65001
 
- Date: 28/05/2024 13:23:09
+ Date: 22/07/2024 23:13:47
 */
 
 SET NAMES utf8mb4;
@@ -31,12 +31,30 @@ CREATE TABLE `admin`  (
   `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '电话',
   `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '邮箱',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '管理员' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '管理员' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of admin
 -- ----------------------------
-INSERT INTO `admin` VALUES (1, 'admin', 'admin', '管理员', 'http://localhost:9090/files/1697438073596-avatar.png', 'ADMIN', '13677889922', 'admin@xm.com');
+INSERT INTO `admin` VALUES (1, 'admin', 'admin', '管理员', 'http://localhost:9090/files/1718184911949-安和昴.jpg', 'ADMIN', '13677889922', 'admin@xm.com');
+INSERT INTO `admin` VALUES (2, '123', '123', '123', NULL, 'ADMIN', '123', '123');
+
+-- ----------------------------
+-- Table structure for banner
+-- ----------------------------
+DROP TABLE IF EXISTS `banner`;
+CREATE TABLE `banner`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `img` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '图片',
+  `business_id` int(0) NULL DEFAULT NULL COMMENT '商家ID',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '广告表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of banner
+-- ----------------------------
+INSERT INTO `banner` VALUES (3, 'http://localhost:9090/files/1721025571150-c129b49efbf437cb0ed6ca4c747dffc662043450.jpg', 1);
+INSERT INTO `banner` VALUES (4, 'http://localhost:9090/files/1721026083236-f9d9bd1b0e48c2eb9a57d28e43e2cd0362043450.jpg', 23);
 
 -- ----------------------------
 -- Table structure for business
@@ -57,14 +75,100 @@ CREATE TABLE `business`  (
   `time_range` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '营业时间',
   `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '类型',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 15 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '商家表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 23 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '商家表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of business
 -- ----------------------------
-INSERT INTO `business` VALUES (1, '123', '123', '汉堡王', NULL, NULL, NULL, NULL, NULL, NULL, '待审核', NULL, NULL);
-INSERT INTO `business` VALUES (14, '星巴克', '123456', '星巴克', NULL, 'BUSINESS', NULL, NULL, NULL, NULL, '待审核', NULL, NULL);
+INSERT INTO `business` VALUES (1, '汉堡王', '123', '汉堡王', NULL, 'BUSINESS', NULL, NULL, NULL, NULL, '通过', NULL, NULL);
+INSERT INTO `business` VALUES (14, '可口可乐', '123456', '可口可乐', NULL, 'BUSINESS', NULL, NULL, NULL, NULL, '拒绝', NULL, '奶茶饮品');
 INSERT INTO `business` VALUES (15, '一点点', '123456', '一点点', NULL, 'BUSINESS', NULL, NULL, NULL, NULL, '待审核', NULL, NULL);
+INSERT INTO `business` VALUES (20, '库迪咖啡', '123', '库迪咖啡', NULL, 'BUSINESS', NULL, NULL, NULL, NULL, '待审核', NULL, '奶茶饮品');
+INSERT INTO `business` VALUES (21, '瑞幸咖啡', '123', '瑞幸咖啡', NULL, 'BUSINESS', NULL, NULL, NULL, NULL, '待审核', NULL, '奶茶饮品');
+INSERT INTO `business` VALUES (22, '塔斯汀', '123', NULL, NULL, 'BUSINESS', NULL, NULL, NULL, NULL, '待审核', NULL, NULL);
+INSERT INTO `business` VALUES (23, '蜜雪冰城', '123', '蜜雪冰城', 'http://localhost:9090/files/1718348742815-安和昴.jpg', 'BUSINESS', NULL, NULL, '翻斗大街翻斗花园二号楼1001室', NULL, '通过', NULL, '奶茶饮品');
+
+-- ----------------------------
+-- Table structure for category
+-- ----------------------------
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '名称',
+  `business_id` int(0) NULL DEFAULT NULL COMMENT '商家ID',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '商品分类' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of category
+-- ----------------------------
+INSERT INTO `category` VALUES (1, '汉堡', 1);
+INSERT INTO `category` VALUES (7, '奶茶', 23);
+
+-- ----------------------------
+-- Table structure for collect
+-- ----------------------------
+DROP TABLE IF EXISTS `collect`;
+CREATE TABLE `collect`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `business_id` int(0) NULL DEFAULT NULL COMMENT '商家ID',
+  `user_id` int(0) NULL DEFAULT NULL COMMENT '用户ID',
+  `time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '收藏时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '用户收藏' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of collect
+-- ----------------------------
+INSERT INTO `collect` VALUES (1, 1, 1, '2024-7-22');
+
+-- ----------------------------
+-- Table structure for comment
+-- ----------------------------
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '评论',
+  `star` double(10, 1) NULL DEFAULT 0.0 COMMENT '评分',
+  `user_id` int(0) NULL DEFAULT NULL COMMENT '用户ID',
+  `business_id` int(0) NULL DEFAULT NULL COMMENT '商家iD',
+  `goods_id` int(0) NULL DEFAULT NULL COMMENT '商品ID',
+  `order_id` int(0) NULL DEFAULT NULL COMMENT '订单ID',
+  `time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `user_id`(`user_id`, `order_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '评价表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of comment
+-- ----------------------------
+INSERT INTO `comment` VALUES (1, '太好吃了', 5.0, 1, 1, 1, 1, '2024-7.22');
+
+-- ----------------------------
+-- Table structure for goods
+-- ----------------------------
+DROP TABLE IF EXISTS `goods`;
+CREATE TABLE `goods`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '名称',
+  `price` decimal(10, 2) NULL DEFAULT NULL COMMENT '价格',
+  `img` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '图片',
+  `descr` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '描述',
+  `origin` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '原材料',
+  `taste` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '口味',
+  `specs` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '规格',
+  `date` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '上架日期',
+  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT '上架' COMMENT '上架状态',
+  `discount` double(11, 1) NULL DEFAULT 1.0 COMMENT '折扣',
+  `business_id` int(0) NULL DEFAULT NULL COMMENT '商家ID',
+  `category_id` int(0) NULL DEFAULT NULL COMMENT '分类ID',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '商品信息' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of goods
+-- ----------------------------
+INSERT INTO `goods` VALUES (2, '1', 1.00, NULL, NULL, NULL, NULL, NULL, '2024-06-25', '上架', 1.0, 23, 7);
 
 -- ----------------------------
 -- Table structure for notice
@@ -85,5 +189,76 @@ CREATE TABLE `notice`  (
 INSERT INTO `notice` VALUES (1, '今天系统正式上线，开始内测', '今天系统正式上线，开始内测', '2023-09-05', 'admin');
 INSERT INTO `notice` VALUES (2, '所有功能都已完成，可以正常使用', '所有功能都已完成，可以正常使用', '2023-09-05', 'admin');
 INSERT INTO `notice` VALUES (3, '今天天气很不错，可以出去一起玩了', '今天天气很不错，可以出去一起玩了', '2023-09-05', 'admin');
+
+-- ----------------------------
+-- Table structure for orders
+-- ----------------------------
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `order_no` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '订单编号',
+  `time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '下单时间',
+  `pay_time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '支付时间',
+  `pay_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '支付方式',
+  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '订单状态',
+  `business_id` int(0) NULL DEFAULT NULL COMMENT '接单商家ID',
+  `user` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '收货人',
+  `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '收货人电话',
+  `address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '送货地址',
+  `user_id` int(0) NULL DEFAULT NULL COMMENT '下单人ID',
+  `amount` decimal(10, 2) NULL DEFAULT NULL COMMENT '订单总价',
+  `discount` decimal(10, 2) NULL DEFAULT NULL COMMENT '优惠金额',
+  `actual` decimal(10, 2) NULL DEFAULT NULL COMMENT '实付款',
+  `comment` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '备注',
+  `cover` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '缩略图',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '名称',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '订单表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of orders
+-- ----------------------------
+INSERT INTO `orders` VALUES (1, '1716083249945939968', '2023-10-22 21:25:04', '2023-10-22 21:25:06', '支付宝', '待收货', 1, '哈哈', '13988779988', '南京西路1229号', 1, 41.00, 4.10, 36.90, '麻辣', 'http://106.54.221.16:9092/files/1697972203437-QQ截图20231022185405.png', '原味特色卤味等2份餐品');
+
+-- ----------------------------
+-- Table structure for orders_item
+-- ----------------------------
+DROP TABLE IF EXISTS `orders_item`;
+CREATE TABLE `orders_item`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `order_id` int(0) NULL DEFAULT NULL COMMENT '订单ID',
+  `goods_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '商品名称',
+  `goods_img` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '商品图片',
+  `price` decimal(10, 2) NULL DEFAULT NULL COMMENT '商品价格',
+  `num` int(0) NULL DEFAULT NULL COMMENT '购买数量',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '订单详情表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of orders_item
+-- ----------------------------
+INSERT INTO `orders_item` VALUES (1, 1, '卤猪蹄', NULL, NULL, NULL);
+INSERT INTO `orders_item` VALUES (2, 1, '炸花生米', NULL, NULL, NULL);
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '账号',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '密码',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '姓名',
+  `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '头像',
+  `role` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '角色',
+  `sex` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '性别',
+  `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '电话',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, '哈哈', '123', '哈哈', 'http://localhost:9090/files/1718184810262-我的.jpg', '男', NULL, '18050328082');
 
 SET FOREIGN_KEY_CHECKS = 1;
