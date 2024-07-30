7502b6b3081945a1facce426cd8e9b81131c8de4
nigeiru <158778430+nigeiru@users.noreply.github.com>
2024-07-30 15:04:54 +0800

    uniapp的三个页面

---
+++ manager/uniapp/pages/orders/orders.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<text>我的订单</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
